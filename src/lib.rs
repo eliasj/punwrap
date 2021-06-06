a9d8b4c3c7c19fabeb65b6efb6909a8e215d4727
Viktor Eikman <viktor.eikman@gmail.com>
2021-06-06 08:45:30 +0200

    Vertical slice

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use runwrap;
+
+/// Wrap each of the interface functions of runwrap.
+#[pyfunction]
+fn wrap(raw: &str, width: usize) -> PyResult<String> {
+    Ok(runwrap::wrap(raw, width))
+}
+
+/// Expose a Python module implemented in Rust.
+#[pymodule]
+fn punwrap(_: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(wrap, m)?)?;
+
+    Ok(())
 }
